2611eabbe8cc20c29ad616aa1165544166f500f8
Noah Lourenço <noah.damian.tech@gmail.com>
2024-03-06 13:51:31 -0300

    project created

---
+++ backend/src/main/java/br/ong/bemparatodos/bemparatodos/BemparatodosApplication.java
@@ -0,0 +1,13 @@
+package br.ong.bemparatodos.bemparatodos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemparatodosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemparatodosApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/ong/bemparatodos/bemparatodos/BemparatodosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ong.bemparatodos.bemparatodos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemparatodosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

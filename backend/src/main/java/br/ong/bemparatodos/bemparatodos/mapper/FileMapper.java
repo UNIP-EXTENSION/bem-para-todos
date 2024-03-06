64217eee3a409c950487de008d3f960530467d5e
Noah Lourenço <noah.damian.tech@gmail.com>
2024-03-06 20:22:03 -0300

    feat(File): Created File Mapper and Record

--- backend/src/main/java/br/ong/bemparatodos/bemparatodos/mapper/FileMapper.java
+++ backend/src/main/java/br/ong/bemparatodos/bemparatodos/mapper/FileMapper.java
@@ -1,7 +1,7 @@
 package br.ong.bemparatodos.bemparatodos.mapper;
 
-import br.ong.bemparatodos.bemparatodos.config.MapperConfiguration;
-import br.ong.bemparatodos.bemparatodos.dto.AddressDTO;
+import br.ong.bemparatodos.bemparatodos.config.mapper.MapperConfiguration;
+import br.ong.bemparatodos.bemparatodos.record.AddressDTO;
 import br.ong.bemparatodos.bemparatodos.entity.Address;
 import org.mapstruct.Mapper;
 
--- backend/src/main/java/br/ong/bemparatodos/bemparatodos/record/FileRecord.java
+++ backend/src/main/java/br/ong/bemparatodos/bemparatodos/record/FileRecord.java
@@ -1,15 +1,30 @@
-package br.ong.bemparatodos.bemparatodos.dto;
+package br.ong.bemparatodos.bemparatodos.record;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.Instant;
 import java.util.UUID;
 
 public record FileRecord(
 
+   @NotNull(message = "The ID cannot be null")
    UUID id,
+
+   @NotBlank(message = "The file name cannot be blank")
    String fileName,
+
+   @NotBlank(message = "The file type cannot be blank")
    String fileType,
+
+   @NotNull(message = "The file size cannot be null")
+   @Min(value = 1, message = "The value must be greater than or equal to 1")
    Long fileSize,
+
    String description,
+
+   @NotNull(message = "The upload date cannot be null")
    Instant uploadDate
 
 ) {}

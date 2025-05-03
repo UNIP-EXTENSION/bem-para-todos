1e3eb82a4600769c7f3302f09226cafc455f892f
SrAbnerP <abner.sol.pereira@gmail.com>
2025-05-02 21:56:13 -0300

    feat: add the http client in auth service

--- frontend/src/services/auth_service.ts
+++ frontend/src/services/auth_service.ts
@@ -1,30 +1,23 @@
 import { AuthStorage } from "../storage/auth_storage";
-import { BASE_URL } from "react-native-dotenv";
+import httpClient from "./http_client";
 
 export class AuthService {
-  private baseUrl = `${BASE_URL}/auth`;
+  private endpoint = "/auth";
 
   // Método para autenticar o usuário
   async login(email: string, password: string): Promise<boolean> {
     try {
-      const response = await fetch(this.baseUrl, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password }),
+      const response = await httpClient.post(this.endpoint, {
+        email,
+        password,
       });
 
-      if (response.status === 200) {
-        const data = await response.json();
-        const token = data.access_token;
-
-        await AuthStorage.saveToken(token);
-        return true;
-      } else {
-        console.warn(`Falha no login: ${response.status}`);
-        return false;
-      }
-    } catch (error) {
-      console.error("Erro ao realizar login:", error);
+      const token = response.data.access_token;
+      await AuthStorage.saveToken(token);
+      return true;
+    } catch (error: any) {
+      const status = error.response?.status || "N/A";
+      console.warn(`Falha no login: ${status}`);
       return false;
     }
   }
